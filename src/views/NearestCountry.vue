3995ddfc764aa28d4000596a7a23681db57f7ee3
jbmay1497 <jared.b.may@vanderbilt.edu>
2022-03-01 01:20:58 -0500

    added routing

---
+++ src/views/NearestCountry.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "NearestCountry"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/WaterOrLand.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "WaterOrLand"
+}
+</script>
+
+<style scoped>
+
+</style>
