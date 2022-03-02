31e5210c355d021180f34ca8f2b9e0d580214036
jbmay1497 <jared.b.may@vanderbilt.edu>
2022-03-01 20:27:19 -0500

    added onwater api

--- src/views/WaterOrLand.vue
+++ src/views/WaterOrLand.vue
@@ -1,5 +1,7 @@
 <template>
-  <h3>Installed CLI Plugins</h3>
+  <div>{{this.latitude}}</div>
+  <div>{{this.longitude}}</div>
+  <div>{{this.water}}</div>
 </template>
 
 <script>
@@ -9,9 +11,17 @@ export default {
    return{
      latitude: null,
      longitude: null,
-     countryData: null
+     water: null
    }
+
  },
+  async created () {
+    this.generateLatitude();
+    this.generateLongitude();
+  },
+  async beforeMount(){
+    await this.getWaterOrLand(this.latitude, this.longitude);
+  },
   methods: {
     generateLatitude() {
       this.latitude = (Math.random() * 90 * (Math.random() < 0.5 ? -1 : 1)).toFixed(2);
@@ -21,6 +31,13 @@ export default {
       this.longitude = (Math.random() * 180 * (Math.random() < 0.5 ? -1 : 1)).toFixed(2);
       return this.longitude
     },
+    async getWaterOrLand(latitude, longitude){
+      const key = '1yuKk6UQrfcBLdx_Afds';
+      let url = `https://api.onwater.io/api/v1/results/${latitude},${longitude}?access_token=${key}`;
+      const res = await fetch(url);
+      let res_json = await res.json();
+      this.water = res_json["water"];
+    }
   }
 }
 </script>
