7fb928a6fc626a20af471fe2c5b6380c29c96f81
jbmay1497 <jared.b.may@vanderbilt.edu>
2022-03-03 00:02:11 -0500

    removed location map

--- src/components/modalComponents/RoundOverModal.vue
+++ src/components/modalComponents/RoundOverModal.vue
@@ -1,5 +1,5 @@
-<template>
-  <div class="modal" v-if="show">
+<template v-if="show">
+  <div class="modal" >
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header" id="map">
